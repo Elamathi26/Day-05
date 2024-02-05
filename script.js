b2901c713930221572324cd248159562b632daa9
Elamathi26 <elamathi2611@gmail.com>
2024-02-05 12:00:19 +0530

    final

---
+++ script.js
@@ -0,0 +1,50 @@
+var myResume={
+    "basics": {
+      "name": "ELAMATHI G",
+      "email": "elamathi2611@gmail.com",
+      "phone": 9361582272,
+      "degree": "M.A",
+      "location": {
+        "address": "93, Pettai Perumal Kovil Street, Anthiyur",
+        "postalCode": 638501,
+        "city": "Erode",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/elamathig2000",
+          "github":"https://github.com/Elamathi26"
+        }
+      ]
+    },
+    "education": [
+      { "institution": "J.K.K.Nataraja College Of Arts and Science",
+        "department": "English Literature",
+        "studyType": "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2023,
+        "gpa": 7.0,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,ms word,ms excel",
+        "level": "beginner",
+        "project": [
+          "Nil"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "content writter,story writing,",
+      }
+    ]
+  }
+  console.log(myResume);
