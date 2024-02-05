e427474e502b57226392193f75795eef25f2a66e
Elamathi26 <elamathi2611@gmail.com>
2024-02-05 13:16:40 +0530

    final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//QUESTION NO: 1
+
 var myResume={
     "basics": {
       "name": "ELAMATHI G",
@@ -48,3 +50,46 @@ var myResume={
     ]
   }
   console.log(myResume);
+
+  //QUESTION NO : 2
+
+  var json = [{
+    "name" : "elamathi1", 
+    "content" : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "task" : "zen portal task",
+    "mail": "elamathi2611@gmail.com"
+},
+
+];
+//for loop
+for(var i=0; i<json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.name);
+    console.log(obj.content);
+    console.log(obj.task);
+    console.log(obj.mail);
+
+}
+//for Each
+json.forEach(function(obj) { console.log(obj.name); });
+json.forEach(function(obj) { console.log(obj.content); });
+json.forEach(function(obj) { console.log(obj.task); });
+json.forEach(function(obj) { console.log(obj.mail); });
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+console.log(json[key].name);
+console.log(json[key].content);
+console.log(json[key].task);
+console.log(json[key].mail);
+ 
+}
+}
+//for Of
+var text = "";
+for (var x of json[key].name) {
+ text += x; 
+}
+ console.log(text);
